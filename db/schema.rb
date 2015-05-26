96ed7b2fbd083e85ee69678d92e26772b3a6d419
Loy <Archloy@users.noreply.github.com>
2015-05-26 09:27:27 +0000

    Adding articles table and forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524155620) do
+ActiveRecord::Schema.define(version: 20150526092507) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer  "owner"
+    t.datetime "date"
+    t.string   "title"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
